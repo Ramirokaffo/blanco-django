105fac926adfe91b444f8ec79c213e07b62c6d9b
RAMIRO KAFFO <ramirokaffo@RAMIROs-MacBook-Air.local>
2026-02-17 02:07:34 +0100

    Correct docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Attente de la disponibilité de MySQL..."
-
-until mysqladmin ping -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
-  echo "MySQL n'est pas encore disponible - attente..."
-  sleep 2
-done
-
-echo "MySQL est disponible - exécution des migrations"
+echo "Exécution des migrations..."
 
 python manage.py migrate --noinput
-
-echo "Démarrage de l'application Django..."
 
 exec "$@"
