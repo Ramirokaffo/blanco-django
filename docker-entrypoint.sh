0707777ba3b6627de5f02ac6bae13e8889801b32
RAMIRO KAFFO <ramirokaffo@RAMIROs-MacBook-Air.local>
2026-02-17 02:28:39 +0100

    Correct docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Attente de MySQL..."
+echo "HOST=$MYSQL_HOST USER=$MYSQL_USER DB=$MYSQL_DATABASE"
 
 until mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
   echo "MySQL pas prêt, attente..."
